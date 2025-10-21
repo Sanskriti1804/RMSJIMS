eda31b75826c617ad1b6e09db48f7cd0f0c2d414
Sanskriti1804 <sanskriti182004@gmail.com>
2025-10-21 14:11:01 +0530

    ims

--- app/src/main/java/com/example/labinventory/data/model/UiState.kt
+++ app/src/main/java/com/example/labinventory/data/model/UiState.kt
@@ -3,6 +3,7 @@ package com.example.labinventory.data.model
 // Sealed classes are used to represent restricted class hierarchies, ensuring that all possible states are known at compile time.
 sealed class UiState<out T>{
     object Loading : UiState<Nothing>()
-    data class Success<out T>(val data: T) : UiState<T>()
+    //data class as they hold data
+    data class Success<out T>(val data: T) : UiState<T>() //T - as it only carries some type of data rest dont
     data class Error(val exception: Throwable) : UiState<Nothing>()
 }
